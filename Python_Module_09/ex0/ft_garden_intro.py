ff9a946ef72f00b8d5ee902cfb669d251c1cb4fe
DanirroT <tribeirinho@GAMEON-PC.localdomain>
2026-02-19 20:47:13 +0000

    17/02

---
+++ Python_Module_09/ex0/ft_garden_intro.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro():
+    """Introduction to Garden Management. Creates Variables for a
+plant: name, height and age. After, Prints the information."""
+    print("=== Welcome to My Garden ===")
+    plant_name = "Rose"
+    height = 25
+    age = 30
+    print("Plant:", plant_name)
+    print("Height:", height)
+    print("Age:", age, "days")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
+    print("\n=== End of Program ===")
